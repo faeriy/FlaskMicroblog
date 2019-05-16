22356d92b59fef5339ac3aafb054adc5e5325efc
faeriy <pleskovych@gmail.com>
2019-05-16 16:59:55 +0300

    Revert "Working bootstrap page with posts from database."
    This reverts commit 56f3f47c90d3de481ca6c02c476ac4f8b74bc934.

--- app.py
+++ app.py
@@ -1,9 +1,33 @@
-from flask import Flask
-from config import Configuration
-from flask_sqlalchemy import SQLAlchemy
-
+# -*- coding: utf-8 -*-
+from flask import Flask, render_template
 
 app = Flask(__name__)
-app.config.from_object(Configuration)
 
-db = SQLAlchemy(app)
+
+@app.route('/')
+@app.route('/index')
+def index():
+    user = {'username': 'Павло'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        },
+        {
+            'author': {'username': 'Ипполит'},
+            'body': 'Какая гадость эта ваша заливная рыба!!'
+        },
+        {
+            'author': {'username': 'Peter Miller'},
+            'body': 'Штефкова валіза - заєбісь!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
+
+
+if __name__ == '__main__':
+    app.run()
--- config.py
+++
@@ -1,4 +0,0 @@
-class Configuration(object):
-    DEBUG = True
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:evohub36@localhost/test1'
